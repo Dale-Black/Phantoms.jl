12817ad2acd22d0afad1b622c77f9cc023aad7e9
Documenter.jl <documenter@juliadocs.github.io>
2022-01-06 21:32:41 +0000

    build based on b6d0d91

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Phantoms","category":"page"},{"location":"#Phantoms","page":"Home","title":"Phantoms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Phantoms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Phantoms]","category":"page"}]
+}
