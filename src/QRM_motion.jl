7fbea3ec20d255643a0e1070ebf1aea3bfed3820
Dale-Black <62632960+Dale-Black@users.noreply.github.com>
2022-08-15 11:10:42 -0700

    Update QRM_motion.jl

--- src/QRM_motion.jl
+++ src/QRM_motion.jl
@@ -75,8 +75,8 @@ function calc_output_motion(
 end
 
 function mask_inserts_motion(dcm_array, slices1, slices2; threshold=115, radius=5)
-	output1 = calc_output_motion(masked_array, header, slices1, threshold)
-	output2 = calc_output_motion(masked_array, header, slices2, threshold)
+	output1 = calc_output_motion(dcm_array, header, slices1, threshold)
+	output2 = calc_output_motion(dcm_array, header, slices2, threshold)
 	center1 = output1[4][1]
 	center2 = output2[4][1]
 
